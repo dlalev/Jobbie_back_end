fd13764b84e90877b0a5f285bc13f20c0f16e60e
dlalev <dimitar.lalev20@gmail.com>
2022-04-10 12:49:39 +0200

    added back end
    java compiler error

---
+++ Jobbie_Spring_Boot/customer/src/main/java/com/jobbie/customer/CustomerService.java
@@ -0,0 +1,14 @@
+package com.jobbie.customer;
+
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        // todo: check if email valid
+        // todo: check if email not taken
+        customerRepository.save(customer);
+    }
+}
